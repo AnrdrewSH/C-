cfa63e44c03ee32c54a9a4365c95b72aec2f39db
Andrew Shvetsov <av-shvesov2015@yandex.ru>
2022-01-08 15:18:59 +0300

    second commit

--- DeadLine/main.cpp
+++ DeadLine/main.cpp
@@ -1,6 +1,16 @@
+/*
+/ Task - Market
+/ ---------------------------------------------------+
+/ Implemented by Andrey Shvetsov, 24.12.2021 3:06 PM /
+/ ---------------------------------------------------+
+*/
+
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set> 
 #include <sstream>
 #include <iterator>
 #include <algorithm>
@@ -8,7 +18,11 @@
 #define INPUT_FILE_NAME "INPUT.txt"
 #define OUTPUT_FILE_NAME "OUTPUT.txt"
 
-struct READING_RESULT
+/*
+/ Thank you very much, i did not know about
+/ this possibility of creating structures.
+*/
+struct
 {
 	const std::string OK = "ok";
 	const std::string WRONG_NUMBER_OF_LINES = "wrong number of lines (must be 3)";
@@ -18,7 +32,7 @@ struct READING_RESULT
 	const std::string BAD_AMOUNT_OF_DATA = "amount of data does not match the one specified in the parameters";
 	const std::string PARAMS_VALUE_OUT_OF_RANGE = "value in the parameters are out of range (1 <= value <= 100000)";
 	const std::string DATA_VALUE_OUT_OF_RANGE = "data contains value out of range (1 <= value <= 100000000)";
-};
+} READING_RESULT;
 
 std::string readInput(std::vector<std::vector<int>>& lines);
 
@@ -32,16 +46,18 @@ int main(int argc, char const *argv[])
 	std::vector<std::vector<int>> lines;
 
 	const std::string readingResult = readInput(lines);
-	if (readingResult != READING_RESULT().OK)
+	if (readingResult != READING_RESULT.OK)
 	{
 		std::cerr << "[error]: " << readingResult << std::endl;
 		return 1;
 	}
 
-	std::vector<int> prices = lines[0];
-	std::sort(prices.begin(), prices.end());
-	prices.erase(unique(prices.begin(), prices.end()), prices.end());
-
+	/*
+	/ Yeah, a good point about sets.
+	/ Thanks again!
+	*/
+	std::set<int> prices(lines[0].begin(), lines[0].end());
+	
 	std::vector<int>& sellers = lines[0];
 	std::sort(sellers.begin(), sellers.end());
 
@@ -52,16 +68,16 @@ int main(int argc, char const *argv[])
 	int uniformPrice = 0;
 	int maxProfit = 0;
 
-	for (int i = 0; i < prices.size(); ++i)
+	for (int price : prices)
 	{
-		int readyToBuy = countReadyToBuy(buyers, prices[i]);
-		int readyToSell = countReadyToSell(sellers, prices[i]);
+		int readyToSell = countReadyToSell(sellers, price);
+		int readyToBuy = countReadyToBuy(buyers, price);
 
-		int profit = (readyToBuy > readyToSell ? readyToSell : readyToBuy) * prices[i];
+		int profit = (readyToSell > readyToBuy ? readyToBuy : readyToSell) * price;
 		if (profit > maxProfit)
 		{
 			maxProfit = profit;
-			uniformPrice = prices[i];
+			uniformPrice = price;
 		}
 	}
 
@@ -77,7 +93,7 @@ int countReadyToSell(std::vector<int>& sellers, int price)
 {
 	for (int i = 0; i < sellers.size(); ++i)
 		if (sellers[i] > price)
-			return i - 1;
+			return i;
 	return sellers.size();
 }
 
@@ -85,7 +101,7 @@ int countReadyToBuy(std::vector<int>& buyers, int price)
 {
 	for (int i = 0; i < buyers.size(); ++i)
 		if (buyers[i] < price)
-			return i - 1;
+			return i;
 	return buyers.size();
 }
 
@@ -103,7 +119,7 @@ std::string readInput(std::vector<std::vector<int>>& lines)
 
 	std::ifstream ifs(INPUT_FILE_NAME);
 	if (!ifs.is_open())
-		return READING_RESULT().CANNOT_OPEN_FILE;
+		return READING_RESULT.CANNOT_OPEN_FILE;
 	
 	int lineIndex = 0;
 	while (std::getline(ifs, line))
@@ -114,17 +130,17 @@ std::string readInput(std::vector<std::vector<int>>& lines)
 		for (int i = 0; i < vec.size(); ++i)
 		{
 			if (!isNumber(vec[i]))
-				return READING_RESULT().INVALID_INPUT_DATA;
+				return READING_RESULT.INVALID_INPUT_DATA;
 			
 			int value = std::stoi(vec[i]);
 
 			if (lineIndex == 0)
 				if (!(value >= 1 && value <= 100000))
-					return READING_RESULT().PARAMS_VALUE_OUT_OF_RANGE;
+					return READING_RESULT.PARAMS_VALUE_OUT_OF_RANGE;
 
 			if (lineIndex > 0)
 				if (!(value >= 1 && value <= 1000000000))
-					return READING_RESULT().DATA_VALUE_OUT_OF_RANGE;
+					return READING_RESULT.DATA_VALUE_OUT_OF_RANGE;
 
 			res[i] = value;
 		}
@@ -134,10 +150,10 @@ std::string readInput(std::vector<std::vector<int>>& lines)
 	ifs.close();
 
 	if (lines.size() != 3)
-		return READING_RESULT().WRONG_NUMBER_OF_LINES;
+		return READING_RESULT.WRONG_NUMBER_OF_LINES;
 
 	if (lines[0].size() != 2)
-		return READING_RESULT().WRONG_PARAMS;
+		return READING_RESULT.WRONG_PARAMS;
 
 	int sellersLength = lines[0][0];
 	int buyersLength = lines[0][1];
@@ -145,7 +161,7 @@ std::string readInput(std::vector<std::vector<int>>& lines)
 	lines.erase(lines.begin());
 
 	if (lines[0].size() != sellersLength || lines[1].size() != buyersLength)
-		return READING_RESULT().BAD_AMOUNT_OF_DATA;
+		return READING_RESULT.BAD_AMOUNT_OF_DATA;
 
-	return READING_RESULT().OK;
+	return READING_RESULT.OK;
 }
